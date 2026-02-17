47ff598fbd7aef38e0ee5b6f1470c7f738ceb4b6
raxit-Cybersync <raxit@cybersync-technologies.com>
2026-02-18 00:35:15 +0530

    project started

---
+++ src/app/page.jsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+   <div>
+    <Button className="flex flex-col items-center justify-center h-screen ">Hello</Button>
+   </div>
+  );
+}
